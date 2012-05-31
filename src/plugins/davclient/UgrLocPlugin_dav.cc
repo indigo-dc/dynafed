6dcd745187eb67d3047b7d32ee86e4ebeae0e8c2
adevress <adevress@cern.ch>
2012-05-31 14:31:52 +0000

    - add modification for notification system

--- src/plugins/davclient/UgrLocPlugin_dav.cc
+++ src/plugins/davclient/UgrLocPlugin_dav.cc
@@ -34,7 +34,7 @@ void UgrLocPlugin_dav::runsearch(struct worktoken *op, int myidx){
 	struct stat st;
 	static const char * fname = "UgrLocPlugin_dav::runsearch";
 	std::string cannonical_name = base_url;
-	bool bad_answer=false;
+	bool bad_answer=true;
 	Davix::DAVIX_DIR* d=NULL;
 
 
@@ -69,86 +69,86 @@ void UgrLocPlugin_dav::runsearch(struct worktoken *op, int myidx){
 			default:
 				break;
 		}
+		bad_answer = false; // reach here -> request complete 
 	} catch (Glib::Error & e) {
 		LocPluginLogInfoThr(SimpleDebug::kHIGH, fname, " UgrDav plugin request Error : " << " name: " <<  g_quark_to_string(e.domain()) << " Catched exception: " << e.code() << " what: " << e.what());
-		bad_answer = true;
 		if(d)
 			dav_core->closedir(d);
-		// return for now -> prototype
-		return;
 	} catch(std::exception & e){
 		LocPluginLogErr(fname, " UgrDav plugin request Error : Unexcepted Error, FATAL  what: " << e.what());		
-		return;
 	} catch(...){
-		LocPluginLogErr(fname, " UgrDav plugin request Error: Unknow Error Fatal  ");		
-		return;		
+		LocPluginLogErr(fname, " UgrDav plugin request Error: Unknow Error Fatal  ");			
 	}
 	
 	LocPluginLogInfoThr(SimpleDebug::kMEDIUM, fname, "Worker: inserting data for " << op->fi->name);
 
 
-        UgrFileItem it;
+	UgrFileItem it;
 
-        // Lock the file instance
-        unique_lock<mutex> l(*(op->fi));
+	// Lock the file instance
+	unique_lock<mutex> l(*(op->fi));
 
-        op->fi->lastupdtime = time(0);
+	op->fi->lastupdtime = time(0);
 
-        switch (op->wop) {
+	if(bad_answer == false){
+		switch (op->wop) {
 
 
-            case LocationPlugin::wop_Stat:
-                    LocPluginLogInfoThr(SimpleDebug::kHIGHEST, fname, "Worker: stat info:" << st.st_size << " " << st.st_mode);
-                    op->fi->size = st.st_size;
-                    //op->fi->lastupdreqtime = st.st_mtime;
-                    op->fi->status_statinfo = UgrFileInfo::Ok;
-                    op->fi->unixflags = st.st_mode | S_IFDIR;;
-                break;
+			case LocationPlugin::wop_Stat:
+					LocPluginLogInfoThr(SimpleDebug::kHIGHEST, fname, "Worker: stat info:" << st.st_size << " " << st.st_mode);
+					op->fi->size = st.st_size;
+					//op->fi->lastupdreqtime = st.st_mtime;
+					op->fi->status_statinfo = UgrFileInfo::Ok;
+					op->fi->unixflags = st.st_mode | S_IFDIR;;
+				break;
 
-            case LocationPlugin::wop_Locate:
-                // disabled
-                return;
-                break;
+			case LocationPlugin::wop_Locate:
+				// disabled
+				return;
+				break;
 
-            case LocationPlugin::wop_List:
+			case LocationPlugin::wop_List:
 				dirent * dent;
 				while((dent = dav_core->readdir(d)) != NULL){
-                    UgrFileItem it;					
-                    LocPluginLogInfoThr(SimpleDebug::kHIGHEST, fname, "Worker: Inserting list " << dent->d_name);
+					UgrFileItem it;					
+					LocPluginLogInfoThr(SimpleDebug::kHIGHEST, fname, "Worker: Inserting list " << dent->d_name);
 					it.name = std::string(dent->d_name);
-                    it.location.clear();					
-                    op->fi->subitems.insert(it);	
+					it.location.clear();					
+					op->fi->subitems.insert(it);	
 				}
-                dav_core->closedir(d);
-                op->fi->status_statinfo = UgrFileInfo::Ok;                
-                op->fi->status_items = UgrFileInfo::Ok;                   
-                break;
+				dav_core->closedir(d);
+				op->fi->status_statinfo = UgrFileInfo::Ok;                
+				op->fi->status_items = UgrFileInfo::Ok;                   
+				break;
 				
-            default:
-                break;
-        }
+			default:
+				break;
+		}
 
-        // We have modified the data, hence set the dirty flag
-        op->fi->dirty = true;
 
-        // Anyway the notification has to be correct, not redundant
-        switch (op->wop) {
+		// We have modified the data, hence set the dirty flag
+		op->fi->dirty = true;
+	}
+	// Anyway the notification has to be correct, not redundant
+	switch (op->wop) {
 
-            case LocationPlugin::wop_Stat:
-                op->fi->notifyStatNotPending();
-                break;
+		case LocationPlugin::wop_Stat:
+			LocPluginLogInfoThr(SimpleDebug::kHIGHEST, fname, "Notify End Stat");  		
+			op->fi->notifyStatNotPending();
+			break;
 
-            case LocationPlugin::wop_Locate:
-                op->fi->notifyLocationNotPending();
-                break;
+		case LocationPlugin::wop_Locate:
+			LocPluginLogInfoThr(SimpleDebug::kHIGHEST, fname, "Notify End Locate");     		
+			op->fi->notifyLocationNotPending();
+			break;
 
-            case LocationPlugin::wop_List:
-                LocPluginLogInfoThr(SimpleDebug::kHIGHEST, fname, "Notify End Listdir");                     
-                op->fi->notifyItemsNotPending();
-                break;
+		case LocationPlugin::wop_List:
+			LocPluginLogInfoThr(SimpleDebug::kHIGHEST, fname, "Notify End Listdir");                     
+			op->fi->notifyItemsNotPending();
+			break;
 
-            default:
-                break;
-        }
+		default:
+			break;
+	}
 
 }
