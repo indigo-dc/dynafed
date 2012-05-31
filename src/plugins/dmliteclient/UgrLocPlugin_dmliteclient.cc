2f0f8c336d4f07c060e8d914ebdd5f8a4e3f72b1
adevress <adevress@cern.ch>
2012-05-31 14:30:55 +0000

    Added new plugin that talks to dmlite as a client

---
+++ src/plugins/dmliteclient/UgrLocPlugin_dmliteclient.cc
@@ -0,0 +1,164 @@
+/** @file   UgrLocPlugin_dmlite.cc
+ * @brief  Plugin that talks to dmlite
+ * @author Fabrizio Furano
+ * @date   Jan 2012
+ */
+#include "UgrLocPlugin_dmliteclient.hh"
+#include "../../PluginLoader.hh"
+#include <time.h>
+
+
+using namespace boost;
+using namespace std;
+
+UgrLocPlugin_dmlite::UgrLocPlugin_dmlite(SimpleDebug *dbginstance, Config *cfginstance, std::vector<std::string> &parms) :
+LocationPlugin(dbginstance, cfginstance, parms) {
+
+    Info(SimpleDebug::kLOW, "UgrLocPlugin_dmlite", "Creating instance named " << name);
+
+    pluginManager = 0;
+    catalog = 0;
+
+    if (parms.size() > 1) {
+        Info(SimpleDebug::kLOW, "UgrLocPlugin_dmlite", "Dmlite cfg: " << parms[1]);
+
+        pluginManager = new dmlite::PluginManager();
+        pluginManager->loadConfiguration(parms[1]);
+        // Catalog
+        catalog = pluginManager->getCatalogFactory()->createCatalog();
+    }
+
+
+};
+
+void UgrLocPlugin_dmlite::runsearch(struct worktoken *op) {
+    const char *fname = "UgrLocPlugin_dmlite::runsearch";
+    struct stat st;
+    std::vector<FileReplica> repvec;
+    dmlite::Directory *d;
+    bool exc = false;
+
+    if (!catalog) return;
+    if (!pluginManager) return;
+    
+    try {
+        switch (op->wop) {
+
+            case LocationPlugin::wop_Stat:
+                LocPluginLogInfo(SimpleDebug::kHIGH, fname, "invoking Stat(" << op->fi->name << ")");
+                st = catalog->stat(op->fi->name);
+                break;
+
+            case LocationPlugin::wop_Locate:
+                LocPluginLogInfo(SimpleDebug::kHIGH, fname, "invoking getReplicas(" << op->fi->name << ")");
+                repvec = catalog->getReplicas(op->fi->name);
+                break;
+
+            case LocationPlugin::wop_List:
+                LocPluginLogInfo(SimpleDebug::kHIGH, fname, "invoking openDir(" << op->fi->name << ")");
+                d = catalog->openDir(op->fi->name);
+                break;
+
+            default:
+                break;
+        }
+    } catch (dmlite::DmException e) {
+        LocPluginLogInfo(SimpleDebug::kMEDIUM, fname, "Catched exception: " << e.code());
+        exc = true;
+    }
+    LocPluginLogInfo(SimpleDebug::kMEDIUM, fname, "Worker: inserting data for " << op->fi->name);
+
+    // Now put the results
+    {
+        UgrFileItem it;
+        unique_lock<mutex> l(*(op->fi));
+
+        op->fi->lastupdtime = time(0);
+
+        switch (op->wop) {
+
+
+            case LocationPlugin::wop_Stat:
+                if (exc) {
+                    op->fi->pending_statinfo = UgrFileInfo::NotFound;
+                    return;
+                }
+                op->fi->size = st.st_size;
+                op->fi->status_statinfo = UgrFileInfo::Ok;
+                op->fi->unixflags = st.st_mode;
+                break;
+
+            case LocationPlugin::wop_Locate:
+                op->fi->status_locations = UgrFileInfo::NotFound;
+                if (exc) return;
+
+                for (vector<FileReplica>::iterator i = repvec.begin();
+                        i != repvec.end();
+                        i++) {
+                    it.name = i->unparsed_location;
+                    it.location.clear();
+                    op->fi->subitems.insert(it);
+                }
+                op->fi->status_locations = UgrFileInfo::Ok;
+                break;
+
+            case LocationPlugin::wop_List:
+                op->fi->status_items = UgrFileInfo::NotFound;
+                if (exc) return;
+
+                dirent *dent;
+                while ((dent = catalog->readDir(d))) {
+                    it.name = dent->d_name;
+                    it.location.clear();
+                    op->fi->subitems.insert(it);
+                }
+                op->fi->status_items = UgrFileInfo::Ok;
+                break;
+
+            default:
+                break;
+        }
+
+        // We have modified the data, hence set the dirty flag
+        op->fi->dirty = true;
+
+        // Anyway the notification has to be correct, not redundant
+        switch (op->wop) {
+
+            case LocationPlugin::wop_Stat:
+                op->fi->notifyStatNotPending();
+                break;
+
+            case LocationPlugin::wop_Locate:
+                op->fi->notifyLocationNotPending();
+                break;
+
+            case LocationPlugin::wop_List:
+                op->fi->notifyItemsNotPending();
+                break;
+
+            default:
+                break;
+        }
+
+
+
+    }
+
+
+}
+
+
+
+
+// ------------------------------------------------------------------------------------
+// Plugin-related stuff
+// ------------------------------------------------------------------------------------
+
+
+// The hook function. GetLocationPluginClass must be given the name of this function
+// for the plugin to be loaded
+
+extern "C" LocationPlugin * GetLocationPlugin(GetLocationPluginArgs) {
+    return (LocationPlugin *)new UgrLocPlugin_dmlite(dbginstance, cfginstance, parms);
+}
---
+++ src/plugins/dmliteclient/UgrLocPlugin_dmliteclient.hh
@@ -0,0 +1,56 @@
+/** @file   UgrLocPlugin_dmlite.hh
+ * @brief  Plugin that talks to dmlite
+ * @author Fabrizio Furano
+ * @date   Feb 2012
+ */
+
+#ifndef LOCATIONPLUGIN_DMLITE_HH
+#define LOCATIONPLUGIN_DMLITE_HH
+
+
+#include "../../LocationPlugin.hh"
+#include <dmlite/dmlite++.h>
+#include <dmlite/dm_exceptions.h>
+
+/** LocationPlugin_dmlite
+ * Plugin that talks to a dmlite catalog
+ *
+ *
+ * by Fabrizio Furano, CERN, Feb 2012
+ */
+class UgrLocPlugin_dmlite : public LocationPlugin {
+protected:
+
+    dmlite::PluginManager *pluginManager;
+    dmlite::Catalog *catalog;
+
+public:
+
+    UgrLocPlugin_dmlite(SimpleDebug *dbginstance, Config *cfginstance, std::vector<std::string> &parms);
+
+    // Start the async stat process
+    //virtual int do_Stat(UgrFileInfo *fi);
+
+    // Start the async location process
+    //virtual int do_Locate(UgrFileInfo *fi);
+
+    // Start the async listing process
+    //virtual int do_List(UgrFileInfo *fi);
+
+    virtual void runsearch(struct worktoken *op);
+};
+
+
+
+// ------------------------------------------------------------------------------------
+// Plugin-related stuff
+// ------------------------------------------------------------------------------------
+
+// The plugin functionality. This function invokes the plugin loader, looking for the
+// plugin where to call the hook function
+LocationPlugin *GetLocationPluginClass(char *pluginPath, GetLocationPluginArgs);
+
+
+
+#endif
+
