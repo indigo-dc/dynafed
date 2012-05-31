83a636c68887a4092f2abc17451174a189421b85
adevress <adevress@cern.ch>
2012-05-31 14:29:05 +0000

    Added new plugin for simple http handling

---
+++ src/plugins/smplHTTP/UgrLocPlugin_simpleHTTP.cc
@@ -0,0 +1,32 @@
+#include "UgrLocPlugin_simpleHTTP.hh"
+#include "../../PluginLoader.hh"
+#include <time.h>
+
+
+using namespace boost;
+using namespace std;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ------------------------------------------------------------------------------------
+// Plugin-related stuff
+// ------------------------------------------------------------------------------------
+
+
+// The hook function. GetLocationPluginClass must be given the name of this function
+// for the plugin to be loaded
+
+extern "C" LocationPlugin *GetLocationPlugin(GetLocationPluginArgs) {
+    return (LocationPlugin *)new UgrLocPlugin_legacylfc(dbginstance, cfginstance, parms);
+}
