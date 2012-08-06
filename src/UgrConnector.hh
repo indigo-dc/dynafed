aa3bd30b19e8c4e3a9b9f082b569e78876dd5891
adevress <adevress@cern.ch>
2012-08-06 14:04:30 +0000

    - correct minor compilation warning

--- src/UgrConnector.hh
+++ src/UgrConnector.hh
@@ -100,7 +100,7 @@ public:
     /// To be called after the ctor to initialize the object.
     /// @param cfgfile Path to the config file to be loaded
     int init(char *cfgfile = 0);
-    bool resetinit() { initdone = false; }
+    bool resetinit() { initdone = false; return true; }
 
     /// Returns a pointer to the item with the list of the locations of the given lfn (ls).
     /// This could be a list of replicas
