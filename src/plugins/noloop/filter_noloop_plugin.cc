2c7a10c5d05a5cf4687adf788564e1187b9f4a02
adevress <adevress@cern.ch>
2014-01-24 16:47:59 +0000

    - Create filter noloop plugin

---
+++ src/plugins/noloop/filter_noloop_plugin.cc
@@ -0,0 +1,92 @@
+#include "filter_noloop_plugin.hh"
+#include <SimpleDebug.hh>
+#include <LocationInfo.hh>
+#include <davix.hpp>
+
+namespace Asio_ip = boost::asio::ip;
+typedef Asio_ip::udp::resolver Resolver;
+typedef Asio_ip::udp::resolver::query ResolverQuery;
+typedef boost::system::error_code Error_code;
+
+
+FilterNoLoopPlugin::FilterNoLoopPlugin(UgrConnector &c, std::vector<std::string> &parms) :
+    FilterPlugin(c, parms)
+{
+  Info(SimpleDebug::kLOW, "FilterNoLoopPlugin", "Filter NoLoopPlugin loaded");
+}
+
+FilterNoLoopPlugin::~FilterNoLoopPlugin(){
+}
+
+
+static void callback_resolve_query(std::vector<Asio_ip::address> & vec, const std::string & hostname,
+                                   const Error_code& ec, Resolver::iterator iter) {
+    if (ec){
+         Info(SimpleDebug::kHIGH, "FilterNoLoopPlugin::callback_resolve_query", "Error during resolution " << ec);
+         return;
+    }
+    Resolver::iterator end;
+    for (; iter != end; ++iter) {
+        Info(SimpleDebug::kLOW, "FilterNoLoopPlugin::callback_resolve_query", " resolution " << hostname << " to " << iter->endpoint().address());
+        vec.push_back(iter->endpoint().address());
+    }
+}
+
+
+static bool is_matching_address(const std::vector< std::vector<Asio_ip::address> > & rep_vec,
+                                int* i,
+                                const std::vector<Asio_ip::address> & cli_vec, const UgrFileItem_replica & elem){
+    bool res = false;
+    if(std::find_first_of(rep_vec[*i].begin(), rep_vec[*i].end(), cli_vec.begin(), cli_vec.end()) != rep_vec[*i].end()){
+        Info(SimpleDebug::kLOW, "FilterNoLoopPlugin::callback_resolve_query", " Loop detected  on " << elem.name << " deletion ");
+        res = true;
+    }
+    (*i)++;
+    return res;
+}
+
+
+static int filter_internal_list(std::deque<UgrFileItem_replica> &replica, const std::vector< std::vector<Asio_ip::address> > & rep_vec,
+                                const std::vector<Asio_ip::address> & cli_vec){
+    int i=0;
+    Info(SimpleDebug::kLOW, "FilterNoLoopPlugin::is_matching_address", " size of replicas " << replica.size() << " size of rep vec" << rep_vec.size() << " size of cli_vec" << cli_vec.size());
+    std::remove_if(replica.begin(), replica.end(), boost::bind(&is_matching_address, rep_vec, &i, cli_vec, _1));
+    return 0;
+}
+
+
+int FilterNoLoopPlugin::filterReplicaList(std::deque<UgrFileItem_replica> &replica, const UgrClientInfo &cli_info){
+    boost::asio::io_service ios;
+    Resolver res(ios);
+    ResolverQuery q_cli(cli_info.ip, "http");
+    std::vector<Asio_ip::address> cli_vec;
+    std::vector< std::vector<Asio_ip::address> > rep_vec;
+    rep_vec.reserve(replica.size());
+    cli_vec.reserve(2);
+
+    res.async_resolve(q_cli, boost::bind(&callback_resolve_query, boost::ref (cli_vec), cli_info.ip, _1, _2));
+
+    for(std::deque<UgrFileItem_replica>::iterator it = replica.begin(); it != replica.end(); ++it){
+       rep_vec.resize(rep_vec.size()+1);
+       Davix::Uri uri(it->name);
+       if(uri.getStatus() != Davix::StatusCode::OK){
+            Info(SimpleDebug::kLOW, "FilterNoLoopPlugin::filterReplicaList", "Invalid replica content " << it->name);
+            continue;
+       }
+       ResolverQuery q_cli(uri.getHost(), "http");
+       res.async_resolve(q_cli, boost::bind(&callback_resolve_query, boost::ref(rep_vec.back()), uri.getHost(),  _1, _2));
+    }
+   ios.run();
+
+   return filter_internal_list(replica, rep_vec, cli_vec);
+}
+
+
+
+
+/**
+ * Hook for the noloop plugin plugin
+ * */
+extern "C" PluginInterface *GetPluginInterface(UgrConnector &c, std::vector<std::string> &parms) {
+    return (PluginInterface *)new FilterNoLoopPlugin(c, parms);
+}
---
+++ src/plugins/noloop/filter_noloop_plugin.hh
@@ -0,0 +1,34 @@
+#ifndef FILTERNOLOOPPLUGIN_HH
+#define FILTERNOLOOPPLUGIN_HH
+
+#include "plugin_interface.hh"
+#include <boost/asio.hpp>
+#include <boost/thread.hpp>
+
+
+enum AddrType{
+     AddrIPv4,
+     AddrIpv6
+};
+
+struct HostAddr{
+
+};
+
+
+class FilterNoLoopPlugin : public FilterPlugin
+{
+public:
+    FilterNoLoopPlugin( UgrConnector & c, std::vector<std::string> & parms);
+    virtual ~FilterNoLoopPlugin();
+
+
+
+    // sort a list of replica
+    // return -1 if error,  0 if ok
+    virtual int filterReplicaList(std::deque<UgrFileItem_replica> &replica, const UgrClientInfo &cli_info);
+
+private:
+};
+
+#endif // FILTERNOLOOPPLUGIN_HH
