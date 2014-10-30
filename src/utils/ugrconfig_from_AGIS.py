4b1c7921ae1bb88a917a023a03a9ecb1ac9ad564
Ivan Calvet <Ivan.Calvet@cern.ch>
2014-10-30 10:36:57 +0000

    Improved the script:     - If there is only one path, it doesn't print the second line     - You can exclude some keywords with the option -e or --exclude     - It prints directly in a ugr.conf file     - You can specify a proxy to test all the url before adding them to the file. Opt ion -p or --proxy

--- src/utils/ugrconfig_from_AGIS.py
+++ src/utils/ugrconfig_from_AGIS.py
@@ -7,16 +7,41 @@
 
 import os, sys
 import urllib2,simplejson
+import getopt
+import pycurl
 
-try:
+TIMEOUT = 5
+
+def main(argv):
+    flags, args = getopt.getopt(argv, "e:p:", ["exclude=","proxy="])
+    option = {}
+    for flag, arg in flags:
+        flag = flag[flag.rfind('-')+1:]
+        if flag == "e":
+            flag = "exclude"
+        elif flag == "p":
+            flag = "proxy"
+        option[flag] = arg
+    exclude = None
+    if "exclude" in option:
+        exclude = flags[0][1].split(',')
+
     req = urllib2.Request("http://atlas-agis-api.cern.ch/request/service/query/get_se_services/?json&flavour=HTTP", None)
     opener = urllib2.build_opener()
     f = opener.open(req)
     res=simplejson.load(f)
+    output = []
     for s in res:
         protocols = []
         if 'r' in s["aprotocols"]:
             for p in s["aprotocols"]['r']:
+                if exclude:
+                    skip = False
+                    for e in exclude:
+                        if e in p[2]:
+                            skip = True
+                    if skip:
+                        continue
                 protocols.append(p[2])
         prefix = os.path.commonprefix(protocols)
         prefix = prefix[:prefix.rfind("/")]
@@ -27,12 +52,51 @@ try:
             impl = s["impl"]
             if not s["impl"]:
                 impl = "???"
-            url = s["endpoint"] + prefix 
-            print """
-###########
+            url = s["endpoint"] + prefix
+            if "proxy" in option:
+                request_url = url + protocols[0][len(prefix):]
+                test = request(request_url,option["proxy"])
+                print "Test for %s: %s" % (url + protocols[0][len(prefix):], test)
+                if not test.startswith("2"):
+                    continue
+            out = """###########
 ## Talk to a %s instance in %s
 ##
-glb.locplugin[]: /usr/local/lib64/ugr/libugrlocplugin_davrucio.so %s 1 %s
-locplugin.%s.pfxmultiply: %s""" % (impl, s["rc_site"], s["rc_site"], url, s["rc_site"], pro)
-except:
-    print "Unexpected error:", sys.exc_info()[0]    
+glb.locplugin[]: /usr/local/lib64/ugr/libugrlocplugin_davrucio.so %s 1 %s""" % (impl, s["rc_site"], s["rc_site"], url)
+            if len(protocols) > 1:
+                out += "\nlocplugin.%s.pfxmultiply:%s" % (s["rc_site"], pro)
+            output.append(out)
+    file = open('ugr.conf', 'w')
+    file.write("\n\n".join(output))
+    file.close()
+    sys.exit(0)
+
+def request(url, proxy):
+    """ Execute a request defined by the action to test an url"""
+    curl = pycurl.Curl()
+    curl.setopt(pycurl.URL, url)
+    curl.setopt(pycurl.CONNECTTIMEOUT, TIMEOUT)
+    curl.setopt(pycurl.TIMEOUT, TIMEOUT)
+    curl.setopt(pycurl.SSL_VERIFYPEER, 1)
+    curl.setopt(pycurl.SSL_VERIFYHOST, 2)
+    curl.setopt(pycurl.FOLLOWLOCATION, 1)
+    curl.setopt(pycurl.WRITEFUNCTION, lambda x: None)
+    if proxy:
+      curl.setopt(pycurl.CAINFO, proxy)
+      curl.setopt(pycurl.SSLCERT, proxy)
+      curl.setopt(pycurl.CAPATH, "/etc/grid-security/certificates")
+
+    exception = ""
+    try:
+        curl.perform()
+        code = curl.getinfo(pycurl.HTTP_CODE)
+        curl.close()
+    except Exception, e:
+        e = str(e)
+        exception = "Exception " + e[e.find('(')+1:e.rfind(')')]
+        code = 0
+    return str(code)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
