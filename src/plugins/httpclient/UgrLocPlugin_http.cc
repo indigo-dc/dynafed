8d0615705efd222fd4e77eb1d107830a417ef3d3
Fabrizio Furano <Fabrizio.Furano@cern.ch>
2014-11-21 13:41:10 +0000

    In the http ping case, set conn timeout and op timeout to half the time between pings

--- src/plugins/httpclient/UgrLocPlugin_http.cc
+++ src/plugins/httpclient/UgrLocPlugin_http.cc
@@ -219,8 +219,8 @@ void UgrLocPlugin_http::load_configuration(const std::string & prefix) {
 
     checker_params = params;
     struct timespec spec_timeout;
-    spec_timeout.tv_sec = this->availInfo.time_interval_ms / 1000;
-    spec_timeout.tv_nsec = (this->availInfo.time_interval_ms - spec_timeout.tv_sec) * 1000000;
+    spec_timeout.tv_sec = max(this->availInfo.time_interval_ms / 1000 / 2, 1);
+    spec_timeout.tv_nsec = 0;
     checker_params.setOperationTimeout(&spec_timeout);
     checker_params.setConnectionTimeout(&spec_timeout);
     // disable KeepAlive for checker
