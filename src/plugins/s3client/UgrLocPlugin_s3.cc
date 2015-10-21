15cb65cbe2379c2b0afbf1cc7c3535fa1e276093
Fabrizio Furano <Fabrizio.Furano@cern.ch>
2015-10-21 13:38:06 +0000

    Rename the parameter s3.alternate and fix the doc. Follow the new things in the davix api.

--- src/plugins/s3client/UgrLocPlugin_s3.cc
+++ src/plugins/s3client/UgrLocPlugin_s3.cc
@@ -434,7 +434,7 @@ void UgrLocPlugin_s3::configure_S3_parameter(const std::string & prefix){
     signature_validity = (time_t)pluginGetParam<long>(prefix, "s3.signaturevalidity", 3600);
     Info(UgrLogger::Lvl1, name, " S3 signature validity is " << signature_validity);
     
-    const bool s3_v2alternate = pluginGetParam<bool>(prefix, "s3.v2alternate", false);
+    const bool s3_alternate = pluginGetParam<bool>(prefix, "s3.alternate", false);
     if (s3_priv_key.size() > 0 && s3_pub_key.size()){
         Info(UgrLogger::Lvl1, name, " S3 authentication defined");
     }
@@ -447,12 +447,12 @@ void UgrLocPlugin_s3::configure_S3_parameter(const std::string & prefix){
         checker_params.setAwsRegion(s3_region);
     }
 
-    if(s3_v2alternate) {
+    if(s3_alternate) {
         Info(UgrLogger::Lvl1, name, " S3 - using v2 alternate");
     }
 
-    params.setAwsv2Alternate(s3_v2alternate);
-    checker_params.setAwsv2Alternate(s3_v2alternate);
+    params.setAwsAlternate(s3_alternate);
+    checker_params.setAwsAlternate(s3_alternate);
 }
 
 // concat URI + path, if it correspond to a bucket name, return false -> error
