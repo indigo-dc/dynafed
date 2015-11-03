2a9a1e92aa5713a490b86d7cb4bffcfc244647a5
Fabrizio Furano <Fabrizio.Furano@cern.ch>
2015-11-03 13:32:02 +0000

    Deeper testing for listdir

--- src/test/testlistdir.cc
+++ src/test/testlistdir.cc
@@ -43,30 +43,61 @@ int main(int argc, char **argv) {
     cout << "Invoking list " << endl;
     string fn = argv[3];
 
-    UgrFileInfo* file_infos=NULL;
+    UgrFileInfo *file_infos=NULL, *file_infos2=NULL;
     UgrClientInfo cli;
     ugr.list(fn, cli, &file_infos);
 
-    std::cout << "testlistdir: Start listing: " << std::endl;
+    std::cout << "-------------------------- testlistdir: Start listing: " << std::endl;
     if (file_infos->getItemsStatus() == UgrFileInfo::Ok) {
            for(std::set<UgrFileItem, UgrFileItemComp>::iterator it = file_infos->subdirs.begin();
 				it != file_infos->subdirs.end();
 				++it)
-				std::cout << "File :  " << it->name << " " << it->location << std::endl;
-    }else if(file_infos->getStatStatus() == UgrFileInfo::NotFound ){
-		std::cerr << "file : " << fn << " " << strerror(ENOENT) << std::endl;
+				std::cout << "Subitem :  " << it->name << " " << it->location << std::endl;
+           
+           std::cout << "------------------------ testlistdir: Now stat-ing all the items: " << std::endl;
+           for(std::set<UgrFileItem, UgrFileItemComp>::iterator it = file_infos->subdirs.begin();
+                                it != file_infos->subdirs.end();
+                                ++it) {
+               string name=fn+"/"+it->name;
+           
+               ugr.stat(name, cli, &file_infos2);
+               switch (file_infos->getItemsStatus()) {
+                   case UgrFileInfo::Ok:
+                       std::cout << "Subitem :  " << file_infos2->name << " Flags: " << file_infos2->unixflags << std::endl;
+                       break;
+                   case UgrFileInfo::NotFound:
+                       std::cout << "file : " << fn << " " << strerror(ENOENT) << std::endl;
+                       return -2;
+                   case UgrFileInfo::Error:
+                       std::cout << "testlistdir: Error" << std::endl;
+                       return -2;
+                   case UgrFileInfo::InProgress:
+                       std::cout << "testlistdir: Bug, still in progress" << std::endl;
+                       return -2;
+                   case UgrFileInfo::NoInfo:
+                       std::cout << "testlistdir: Bug, No Information" << std::endl;
+                       return -2;
+                   default:
+                       std::cout << " Unknow FATAL Error ! " << std::endl;
+                       return -2;
+               
+               }
+           }
+           
+    } else if(file_infos->getItemsStatus() == UgrFileInfo::NotFound ){
+		std::cout << "file : " << fn << " " << strerror(ENOENT) << std::endl;
 		return -1;
-	}else{
-        if(file_infos->getStatStatus() == UgrFileInfo::Error){
-            std::cerr << "testlistdir: Error" << std::endl;
+	} else {
+        if(file_infos->getItemsStatus() == UgrFileInfo::Error){
+            std::cout << "testlistdir: Error" << std::endl;
         }
-        if(file_infos->getStatStatus() == UgrFileInfo::InProgress){
-           std::cerr << "testlistdir: Bug, still in progress" << std::endl;
+        if(file_infos->getItemsStatus() == UgrFileInfo::InProgress){
+           std::cout << "testlistdir: Bug, still in progress" << std::endl;
         }
-        if(file_infos->getStatStatus() == UgrFileInfo::NoInfo){
-           std::cerr << "testlistdir: Bug, No Information" << std::endl;
+        if(file_infos->getItemsStatus() == UgrFileInfo::NoInfo){
+           std::cout << "testlistdir: Bug, No Information" << std::endl;
         }
-		std::cerr << " Unknow FATAL Error ! " << std::endl;
+		std::cout << " Unknow FATAL Error ! " << std::endl;
 		return -2;
 	}
 	
