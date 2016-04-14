8d7a440767c2252afe5d69eba868f0782e9e7377
Fabrizio Furano <Fabrizio.Furano@cern.ch>
2016-04-14 12:16:54 +0000

    Betterr example for the pythhon auth plugin

--- src/plugins/authpython/ugrauth_example.py
+++ src/plugins/authpython/ugrauth_example.py
@@ -5,6 +5,11 @@
 # to be authorized
 # usage:
 # ugrauth_example.py clientname remoteaddr <fqan1> .. <fqanN>
+#
+# Return value means:
+# 0 --> access is GRANTED
+# nonzero --> access is DENIED
+#
 
 import sys
 def isallowed(clientname="unknown", remoteaddr="nowhere", resource="none", mode="0", fqans=None, keys=None):
@@ -12,11 +17,22 @@ def isallowed(clientname="unknown", remoteaddr="nowhere", resource="none", mode=
     print "remote address", remoteaddr
     print "fqans", fqans
     print "keys", keys
+    print "mode", mode
     print "resource", resource
-    
-    return 1234
+
+    # Read/list modes are always open
+    if (mode == 'r') or (mode == 'l'):
+      return 0
+
+    # deny to anonymous user for any write mode
+    if (clientname == 'nobody'):
+      return 1
+
+    # allow writing to anyone else who is not nobody
+    return 0
 
 
 #------------------------------
 if __name__ == "__main__":
-    isallowed(sys.argv[1], sys.argv[2:])
+    r = isallowed(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5:])
+    sys.exit(r)
