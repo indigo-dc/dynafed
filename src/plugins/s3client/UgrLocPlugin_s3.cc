c530db4e46cd6098499e724687797b7dbeafb1d8
Fabrizio Furano <Fabrizio.Furano@cern.ch>
2017-03-13 16:25:12 +0000

    Switch the S3 sanity check away from the nasty config file templates.

--- src/plugins/s3client/UgrLocPlugin_s3.cc
+++ src/plugins/s3client/UgrLocPlugin_s3.cc
@@ -436,14 +436,14 @@ void UgrLocPlugin_s3::configure_S3_parameter(const std::string & prefix){
     
     // Now abort everything if the signature validity clashes with the settings of the cache
     // This thing is very important, hence no cache parameter means bad  
-    time_t ttl = (time_t)pluginGetParam<long>(prefix, "extcache.memcached.ttl", 1000000);
+    time_t ttl = (time_t)UgrCFG->GetLong("extcache.memcached.ttl", 1000000);
     if (signature_validity < ttl-60) {
       Error(name, " The given signature validity of " << signature_validity <<
       " is not compatible with the expiration time of the external cache extcache.memcached.ttl (" << ttl << ")");
       throw 1;
     }
     
-    ttl = (time_t)pluginGetParam<long>(prefix, "infohandler.itemmaxttl", 1000000);
+    ttl = (time_t)UgrCFG->GetLong("infohandler.itemmaxttl", 1000000);
     if (signature_validity < ttl-60) {
       Error(name, " The given signature validity of " << signature_validity <<
       " is not compatible with the expiration time of the internal cache infohandler.itemmaxttl (" << ttl << ")");
