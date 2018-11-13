f6a073e4388b1b7f736d5ea8152e0d13487b2d2e
Fabrizio Furano <Fabrizio.Furano@cern.ch>
2018-11-13 08:45:52 +0000

    Adding the tpc push/pull scripts

---
+++ src/utils/ugrpullscript_gfal.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#
+# This script performs a remote file copy using gfal-copy
+#
+
+echo "Starting a PULL TPC ckcheck:$1 cktype:$2 src:$3 dst:$4 delgproxy:$5 xferhdrauth:$6 usr1:$7 usr2:$8 usr3:$9"
+
+# Some debug output
+#mount
+#df
+#ls /
+
+# Note this may breaks gfal, depending on the version
+# If the bearer token field comes from the TransferHeaderAuthorization header field
+# then we splat it into this envvar. This is plain wrong on the side of gfal,
+# as it assumes that there is only one token, while a transfer may need two
+if [ "x$6" != "x" ]; then
+  echo "Setting BEARER_TOKEN to '$6'"
+  export BEARER_TOKEN="$6"
+fi
+
+cmd="gfal-copy -vv --just-copy --cert $5 --key $5 $3 $4"
+echo "command: $cmd"
+$cmd 2>&1
+res=$?
+echo "res: $res"
+
+if [ "x$res" != "x0" ]; then
+  echo "Pull TPC failed. Deleting local attempt '$4'"
+fi
+
+echo "finished"
+exit $res
