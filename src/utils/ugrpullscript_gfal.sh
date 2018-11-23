9b5483f92d459a0092f054f47a1a230693779a66
Fabrizio Furano <Fabrizio.Furano@cern.ch>
2018-11-23 16:17:41 +0000

    Last version of the push/pull scripts

--- src/utils/ugrpullscript_gfal.sh
+++ src/utils/ugrpullscript_gfal.sh
@@ -5,6 +5,7 @@
 #
 
 echo "Starting a PULL TPC ckcheck:$1 cktype:$2 src:$3 dst:$4 delgproxy:$5 xferhdrauth:$6 usr1:$7 usr2:$8 usr3:$9"
+src=$3
 
 # Some debug output
 #mount
@@ -16,8 +17,11 @@ echo "Starting a PULL TPC ckcheck:$1 cktype:$2 src:$3 dst:$4 delgproxy:$5 xferhd
 # then we splat it into this envvar. This is plain wrong on the side of gfal,
 # as it assumes that there is only one token, while a transfer may need two
 if [ "x$6" != "x" ]; then
-  echo "Setting BEARER_TOKEN to '$6'"
-  export BEARER_TOKEN="$6"
+  btk=${6:7}
+  echo "We got a nice bearer token '$btk'"
+  src="$src?authz=$btk"
+  #echo "Setting BEARER_TOKEN to '$btk'"
+  #export BEARER_TOKEN="$btk"
 fi
 
 # NOTE: if the parameter glb.filepullhook.usereplicaurl is false (default)
@@ -29,7 +33,12 @@ fi
 # In the latter case, if any S3 bucket is federated, it will not be possible to
 # pull files larger than 5GB (which is an S3 limit)
 # 
-cmd="gfal-copy -vv --just-copy --cert $5 --key $5 $3 https://fab-dynafed-dev1.cern.ch/$4"
+if [ "x$5" == "x" ]; then
+  cmd="gfal-copy -vv --just-copy $src https://fab-dynafed-dev1.cern.ch/$4"
+else
+  cmd="gfal-copy -vv --just-copy --cert $5 --key $5 $src https://fab-dynafed-dev1.cern.ch/$4"
+fi
+
 echo "command: $cmd"
 $cmd "-DHTTP PLUGIN:ENABLE_REMOTE_COPY=false" 2>&1
 res=$?
--- src/utils/ugrpushscript_gfal.sh
+++ src/utils/ugrpushscript_gfal.sh
@@ -5,6 +5,7 @@
 #
 
 echo "Starting a PUSH TPC ckcheck:$1 cktype:$2 src:$3 dst:$4 delgproxy:$5 xferhdrauth:$6 usr1:$7 usr2:$8 usr3:$9"
+dst=$4
 
 #sleep 5 
 
@@ -17,16 +18,21 @@ echo "Starting a PUSH TPC ckcheck:$1 cktype:$2 src:$3 dst:$4 delgproxy:$5 xferhd
 # If the bearer token field comes from the TransferHeaderAuthorization header field
 # then we splat it into this envvar. This is plain wrong on the side of gfal,
 # as it assumes that there is only one token, while a transfer may need two
+
 if [ "x$6" != "x" ]; then
-  echo "Setting BEARER_TOKEN to '$6'"
-  export BEARER_TOKEN="$6"
+  btk=${6:7}
+  echo "We got a nice bearer token '$btk'"
+  dst="$dst?authz=$btk"
+  #echo "Setting BEARER_TOKEN to '$btk'"
+  #export BEARER_TOKEN="$btk"
 fi
+
 
 if [ "x$5" == "x" ]
 then
-  cmd="gfal-copy -vv --just-copy $3 $4"
+  cmd="gfal-copy -vv --just-copy $3 $dst"
 else
-  cmd="gfal-copy -vv --just-copy --cert $5 --key $5 $3 $4"
+  cmd="gfal-copy -vv --just-copy --cert $5 --key $5 $3 $dst"
 fi
 
 echo "command: $cmd"
@@ -35,8 +41,9 @@ res=$?
 echo "res: $res"
 
 if [ "x$res" != "x0" ]; then
-  echo "Push TPC failed. Deleting local attempt '$4'"
+  echo "Pull TPC failed. Deleting local attempt '$dst'"
 fi
 
 echo "finished"
 exit $res
+
