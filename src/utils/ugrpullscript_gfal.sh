dd844c3aaac7ba377452e6dac82f23f48ffd6ed0
Fabrizio Furano <Fabrizio.Furano@cern.ch>
2018-11-23 10:16:07 +0000

    Correctly pass to gfal-copy the ugly -DHTTP PLUGIN:ENABLE_REMOTE_COPY=false parameter

--- src/utils/ugrpullscript_gfal.sh
+++ src/utils/ugrpullscript_gfal.sh
@@ -29,9 +29,9 @@ fi
 # In the latter case, if any S3 bucket is federated, it will not be possible to
 # pull files larger than 5GB (which is an S3 limit)
 # 
-cmd="gfal-copy -vv --just-copy --cert $5 --key $5 $3 https://myugr.mydomain.com/$4"
+cmd="gfal-copy -vv --just-copy --cert $5 --key $5 $3 https://fab-dynafed-dev1.cern.ch/$4"
 echo "command: $cmd"
-$cmd 2>&1
+$cmd "-DHTTP PLUGIN:ENABLE_REMOTE_COPY=false" 2>&1
 res=$?
 echo "res: $res"
 
